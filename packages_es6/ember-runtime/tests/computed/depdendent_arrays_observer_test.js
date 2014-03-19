93531f11d01e19d3eb6169fa04adc8d53916f500
Matthew Beale <matt.beale@madhatted.com>
2014-03-19 00:14:20 -0400

    Extract DependentArraysObserver into its own module

---
+++ packages_es6/ember-runtime/tests/computed/depdendent_arrays_observer_test.js
@@ -0,0 +1,7 @@
+import DependentArraysObserver from 'ember-runtime/computed/dependent_arrays_observer';
+
+module('DependentArraysObserver');
+
+test("it exists", function(){
+  ok(DependentArraysObserver, "it exists");
+});
