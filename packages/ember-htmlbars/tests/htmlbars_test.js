7afa99672eaf58018c180498d37bf4791e263f42
Matthew Beale <matt.beale@madhatted.com>
2014-10-18 15:07:09 -0400

    WIP: Use the Handlebars bind helper, all but pushChildView working

--- packages/ember-htmlbars/tests/htmlbars_test.js
+++ packages/ember-htmlbars/tests/htmlbars_test.js
@@ -1,23 +1,11 @@
 import { compile } from "htmlbars-compiler/compiler";
 import { defaultEnv } from "ember-htmlbars";
-
-function fragmentHTML(fragment) {
-  var html = '', node;
-  for (var i = 0, l = fragment.childNodes.length; i < l; i++) {
-    node = fragment.childNodes[i];
-    if (node.nodeType === 3) {
-      html += node.nodeValue;
-    } else {
-      html += node.outerHTML;
-    }
-  }
-  return html;
-}
+import { equalHTML } from "./helpers";
 
 QUnit.module("ember-htmlbars");
 
-test("hello world", function() {
+test("HTMLBars is present and can be executed", function() {
   var template = compile("ohai");
-  var output = template({name: 'erik'}, defaultEnv, document.body);
-  equal(fragmentHTML(output), "ohai");
+  var output = template({}, defaultEnv, document.body);
+  equalHTML(output, "ohai");
 });
