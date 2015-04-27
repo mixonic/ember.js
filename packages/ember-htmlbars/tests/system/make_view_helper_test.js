d12e6548b4a1dff6405697e74c653978bdf26f58
Matthew Beale <matt.beale@madhatted.com>
2015-04-26 22:18:14 -0400

    WIP

--- packages/ember-htmlbars/tests/system/make_view_helper_test.js
+++ packages/ember-htmlbars/tests/system/make_view_helper_test.js
@@ -1,13 +1,38 @@
 import makeViewHelper from "ember-htmlbars/system/make-view-helper";
+import EmberView from "ember-views/views/view";
+import { compile } from "ember-template-compiler";
+import Registry from "container/registry";
+import { runAppend, runDestroy } from "ember-runtime/tests/utils";
 
-QUnit.module("ember-htmlbars: makeViewHelper");
+var registry, container, view;
+
+QUnit.module("ember-htmlbars: makeViewHelper", {
+  setup() {
+    registry = new Registry();
+    container = registry.container();
+    registry.optionsForType('helper', { instantiate: false });
+  },
+  teardown() {
+    runDestroy(view);
+  }
+});
 
 QUnit.skip("makes helpful assertion when called with invalid arguments", function() {
-  var viewClass = { toString() { return 'Some Random Class'; } };
+  var SomeRandom = EmberView.extend({
+    template: compile("Some Random Class"),
+    someRandom: true // remove, just a brand
+  });
 
-  var helper = makeViewHelper(viewClass);
+  var helper = makeViewHelper(SomeRandom);
+  registry.register('helper:some-random', helper);
+
+  view = EmberView.create({
+    template: compile("{{some-random 'sending-params-to-view-is-invalid'}}"),
+    container,
+    wrappingView: true // remove, just a brand
+  });
 
   expectAssertion(function() {
-    helper.helperFunction(['foo'], {}, {}, {});
+    runAppend(view);
   }, "You can only pass attributes (such as name=value) not bare values to a helper for a View found in 'Some Random Class'");
 });
