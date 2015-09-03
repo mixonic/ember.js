0e20a5fffbda40cffc31f28624514d5761edc8bf
Sergio Arbeo <serabe@gmail.com>
2015-09-03 13:40:06 +0200

    Extract component keyword to element component
    Current component helper implementation has been moved to element component.

--- packages/ember-htmlbars/lib/keywords/component.js
+++ packages/ember-htmlbars/lib/keywords/component.js
@@ -3,7 +3,7 @@
   @submodule ember-templates
   @public
 */
-import assign from 'ember-metal/assign';
+import { keyword } from 'htmlbars-runtime/hooks';
 
 /**
   The `{{component}}` helper lets you add instances of `Ember.Component` to a
@@ -52,38 +52,12 @@ import assign from 'ember-metal/assign';
   @for Ember.Templates.helpers
   @public
 */
-export default {
-  setupState(lastState, env, scope, params, hash) {
-    let componentPath = env.hooks.getValue(params[0]);
-    return assign({}, lastState, { componentPath, isComponentHelper: true });
-  },
-
-  render(morph, ...rest) {
-    let state = morph.getState();
-
-    if (state.manager) {
-      state.manager.destroy();
-    }
-
-    // Force the component hook to treat this as a first-time render,
-    // because normal components (`<foo-bar>`) cannot change at runtime,
-    // but the `{{component}}` helper can.
-    state.manager = null;
-
-    render(morph, ...rest);
-  },
-
-  rerender: render
-};
-
-function render(morph, env, scope, params, hash, template, inverse, visitor) {
-  let componentPath = morph.getState().componentPath;
-
-  // If the value passed to the {{component}} helper is undefined or null,
-  // don't create a new ComponentNode.
-  if (componentPath === undefined || componentPath === null) {
-    return;
+export default function(morph, env, scope, params, hash, template, inverse, visitor) {
+  if (morph) {
+    keyword('@element_component', morph, env, scope, params, hash, template, inverse, visitor);
+    return true;
   }
 
-  env.hooks.component(morph, env, scope, componentPath, params, hash, { default: template, inverse }, visitor);
+  return true; // Until we have implemented the contextual component
 }
+
